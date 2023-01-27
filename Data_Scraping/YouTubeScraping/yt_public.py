cd8edd20cef7e35d20ddbf24bd5edb54660e21ce
stece <stecenkodasha97@gmail.com>
2023-01-27 23:13:12 +0100

    add last update

--- Data_Scraping/YouTubeScraping/yt_public.py
+++ Data_Scraping/YouTubeScraping/yt_public.py
@@ -4,7 +4,8 @@ from googleapiclient.discovery import build
 from iteration_utilities import unique_everseen
 import csv
 
-from utils.comments import process_comments, make_csv
+from Data_Scraping.YouTubeScraping.utils.comments import process_comments, make_csv
+#from Data_Scraping.YouTubeScraping.utils.comments import process_comments, make_csv
 
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
